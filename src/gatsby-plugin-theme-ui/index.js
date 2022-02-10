ccb43988b61642b6509f9721bb12e9aaaf83c00b
Christian Augustyn <30349879+chrisaugu@users.noreply.github.com>
2022-02-11 08:14:44 +1000

    working on dark mode theme

---
+++ src/gatsby-plugin-theme-ui/index.js
@@ -0,0 +1,14 @@
+// example theme file
+const theme = {
+  fonts: {
+    body: 'system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", sans-serif',
+    heading: '"Avenir Next", sans-serif',
+    monospace: 'Menlo, monospace',
+  },
+  colors: {
+    text: '#000',
+    background: '#fff',
+    primary: '#33e',
+  },
+}
+export default theme
